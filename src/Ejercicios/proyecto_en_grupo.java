0b815a0ec755d008250054ce7eddd0b85b7d5f93
ruben1421 <rubenantoniomunoz0@gmail.com>
2024-12-19 16:06:28 +0100

    Parte 3 del Proyecto
    Permitir al usuario buscar un número en la matriz. Si el número está presente, mostrar su posición (fila y columna). Si no, informar que no existe.

--- src/Ejercicios/proyecto_en_grupo.java
+++ src/Ejercicios/proyecto_en_grupo.java
@@ -1,10 +1,46 @@
 package Ejercicios;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class proyecto_en_grupo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		  Random r = new Random();
+		  Scanner s = new Scanner(System.in);
+		
+		System.out.println("introduce la fila (0 a 4)");
+		int fila = s.nextInt();
+		s.nextLine();
 
+		while(fila >= matrix.length) {
+		if(fila < matrix.length) {
+		break;
+		}
+		System.err.println("ERROR: SOLO UN NUMERO DE 0 A 4");
+		System.out.println("introduce la fila (o a 4)");
+		fila = s.nextInt();
+		s.nextLine();
+		}
+
+		System.out.println("introduce la columna (o a 4)");
+		int columna = s.nextInt();
+		s.nextLine();
+
+		while(columna >= matrix.length) {
+		if(columna < matrix.length) {
+		break;
+		}
+		System.err.println("ERROR: SOLO UN NUMERO DE 0 A 4");
+		System.out.println("introduce la columna (o a 4)");
+		columna = s.nextInt();
+		s.nextLine();
+		}
+
+		System.out.println();
+		System.out.println(matrix[fila][columna]);
+		System.out.println();
 	}
 
 }
