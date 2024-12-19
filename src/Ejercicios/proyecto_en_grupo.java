a41ae95f1282ab043f3935941d430920d1b49e82
HoovyBeast05 <hussein05.ae@alumnes.ilerna.com>
2024-12-19 16:59:07 +0100

    parte 4

--- src/Ejercicios/proyecto_en_grupo.java
+++ src/Ejercicios/proyecto_en_grupo.java
@@ -3,7 +3,18 @@ package Ejercicios;
 public class proyecto_en_grupo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int suma = 0;
+
+		for(int i = 0; i < matrix.length; i++){
+		for(int j = 0; j < matrix[i].length; j++){
+		suma += matrix[i][j];
+		}
+		}
+
+		System.out.println("Suma de todos los numeros en el matriz: " + suma);
+
+		s.close();
 
 	}
 
